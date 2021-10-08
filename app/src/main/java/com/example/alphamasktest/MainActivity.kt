89c094205a73b57cee3209ff841e361da6b1bd10
João Santos <joaomarcelo.ms@gmail.com>
2021-10-08 14:53:00 -0300

    Teste

--- app/src/main/java/com/example/alphamasktest/MainActivity.kt
+++ app/src/main/java/com/example/alphamasktest/MainActivity.kt
@@ -1,65 +1,104 @@
 package com.example.alphamasktest
 
+import android.content.Context
 import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import android.graphics.Bitmap
+
+import androidx.core.graphics.drawable.DrawableCompat
+
+import android.os.Build
+
+import androidx.core.content.ContextCompat
+
+import android.graphics.drawable.Drawable
+import android.animation.Animator
+
+import android.animation.ObjectAnimator
+
+import android.graphics.drawable.RotateDrawable
+
+import android.graphics.drawable.LayerDrawable
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAnimator: ObjectAnimator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         canvasGambi()
+//        animateButton()
+
     }
 
     private fun canvasGambi() {
-
-        val img = findViewById<ImageView>(R.id.mask)
+        val img = findViewById<ImageView>(R.id.imageView)
 
         val canvas = Canvas()
-         val fullRect by lazy { Rect(0, 0, canvas.width, canvas.height) }
 
-        val mask =
-            BitmapFactory.decodeResource(applicationContext.resources, R.mipmap.planta_mask)
-
-//        val mainImage =
-//            BitmapFactory.decodeResource(applicationContext.resources, R.drawable.illustration)
-        val result = Bitmap.createBitmap(218, 138, Bitmap.Config.ARGB_8888)
-//        val resultM = Bitmap.createBitmap(218, 138, Bitmap.Config.ARGB_8888)
+        val mainImage = getBitmapFromVectorDrawable(R.drawable.illustration)
+        val mask = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.planta_mask)
+        val result = Bitmap.createBitmap(mainImage.width, mainImage.height, Bitmap.Config.ARGB_8888)
 
         canvas.setBitmap(result)
-//        canvasM.setBitmap(resultM)
-//        val imagePaint = Paint()
-        val maskPaint = Paint()
+        val paint = Paint()
+        paint.setFilterBitmap(false);
+        canvas.drawBitmap(mainImage, 0f, 0f, paint)
+        paint.setXfermode( PorterDuffXfermode (PorterDuff.Mode.DST_IN))
 
+        canvas.drawBitmap(mask,0f,0f,paint)
+        paint.setXfermode(null);
 
-        maskPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SCREEN)
-//        imagePaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_OVER)
-        val saturation = creatingColorMatrix()
+        img.setImageBitmap(result);
+        img.invalidate();
 
-        maskPaint.setColorFilter(saturation)
-
-        canvas.drawBitmap(mask, 0f, 0f, maskPaint)
-//        canvasM.drawBitmap(mask, 0f, 0f, maskPaint)
-
-
-        result.setHasAlpha(true)
-        canvas.save()
-        img.setImageBitmap(result)
-//        mas.background(resultM)
-
-        img.invalidate()
-        canvas.restore()
     }
 
-    fun creatingColorMatrix(): ColorMatrixColorFilter {
-        val colorMatrix = ColorMatrix()
-        val colorMatrixFilter = ColorMatrixColorFilter(colorMatrix)
-        colorMatrix.setSaturation(0.5f)
-        return colorMatrixFilter
+    fun getBitmapFromVectorDrawable(drawableId: Int): Bitmap {
+        val drawable = ContextCompat.getDrawable(applicationContext, drawableId)
+        val bitmap = Bitmap.createBitmap(
+            drawable!!.intrinsicWidth,
+            drawable.intrinsicHeight, Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+        drawable.draw(canvas)
+        return bitmap
+    }
+
+    private fun animateButton() {
+        val img = findViewById<ImageView>(R.id.imageView)
+
+        val layerDrawable: LayerDrawable = img.getDrawable() as LayerDrawable
+        val drawable = layerDrawable.getDrawable(0) as RotateDrawable
+        mAnimator = ObjectAnimator.ofInt(drawable, "level", 0, 10000)
+        mAnimator.duration = 300
+        mAnimator.start()
+        mAnimator.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animator: Animator) {
+                // do nothing
+            }
+
+            override fun onAnimationEnd(animator: Animator) {
+                img.setVisibility(View.GONE)
+                img.setVisibility(View.VISIBLE)
+            }
+
+            override fun onAnimationCancel(animator: Animator) {
+                Log.v("animation", "onAnimationCancel")
+            }
+
+            override fun onAnimationRepeat(animator: Animator) {
+                // do nothing
+            }
+        })
     }
 
 }
