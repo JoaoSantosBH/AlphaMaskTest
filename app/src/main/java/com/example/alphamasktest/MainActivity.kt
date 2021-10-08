61b44ccf139ff1138d07a7211c591c76f1def859
João Santos <joaomarcelo.ms@gmail.com>
2021-10-08 19:40:28 -0300

    Teste

--- app/src/main/java/com/example/alphamasktest/MainActivity.kt
+++ app/src/main/java/com/example/alphamasktest/MainActivity.kt
@@ -3,53 +3,63 @@ package com.example.alphamasktest
 import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.ImageView
 import android.graphics.Bitmap
 
 import androidx.core.content.ContextCompat
 
 import android.animation.Animator
-
 import android.animation.ObjectAnimator
-
+import android.graphics.drawable.Drawable
 import android.graphics.drawable.RotateDrawable
 
-import android.graphics.drawable.LayerDrawable
+
 import android.util.Log
 
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var mAnimator: ObjectAnimator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         cropAlphaMask()
-        animateButton()
-
     }
 
     private fun cropAlphaMask() {
-        val myImageView = findViewById<ImageView>(R.id.imageView)
+        val myImageView = findViewById<ImageView>(R.id.cardColor)
         val canvas = Canvas()
         val paint = Paint()
+
         val cropedImage = getBitmapFromVectorDrawable(R.drawable.illustration)
         val mask = getBitmapFromVectorDrawable(R.drawable.ic_shape)
 //        val mask = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.ic_shape)
-        val result = Bitmap.createBitmap(cropedImage.width, cropedImage.height, Bitmap.Config.ARGB_8888)
+        val result =
+            Bitmap.createBitmap(cropedImage.width, cropedImage.height, Bitmap.Config.ARGB_8888)
 
         canvas.setBitmap(result)
-        paint.setFilterBitmap(false);
-        canvas.drawBitmap(cropedImage, 0f, 0f, paint)
-        paint.setXfermode( PorterDuffXfermode (PorterDuff.Mode.DST_IN))
-        canvas.drawBitmap(mask,0f,0f,paint)
-        paint.setXfermode(null);
+        paint.isFilterBitmap = false
 
-        myImageView.setImageBitmap(result);
-        myImageView.invalidate();
+        canvas.drawBitmap(cropedImage, 0f, 0f, paint)
+        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_IN)
+        canvas.drawBitmap(mask, 0f, 0f, paint)
+
+        myImageView.setImageBitmap(result)
+        animateShapeLayer(myImageView)
+
+        myImageView.invalidate()
+
+        //ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        //Resources r = getResources();
+        //Drawable[] layers = new Drawable[2];
+        //layers[0] = r.getDrawable(R.drawable.your_background_res);
+        //PulseDrawable pulseDrawable = new PulseDrawable(Color.WHITE);
+        //layers[1] = pulseDrawable;
+        //LayerDrawable layerDrawable = new LayerDrawable(layers);
+        //// This will adjust X & Y of layer, we have to pass layer index (250 pixels from Left and 150 pixels from Right)
+        //layerDrawable.setLayerInset(1, 250, 150, 0, 0);
+        //imageView.setImageDrawable(layerDrawable);
+
     }
 
     fun getBitmapFromVectorDrawable(drawableId: Int): Bitmap {
@@ -64,24 +74,22 @@ class MainActivity : AppCompatActivity() {
         return bitmap
     }
 
-    private fun animateButton() {
+    private fun animateShapeLayer(drawable2: ImageView) {
+        val drawable = findViewById<ImageView>(R.id.cardColor)
+        
+//        val layerDrawable =
+//            ResourcesCompat.getDrawable(resources, R.drawable.canvas_layer, null) as LayerDrawable?
+//        val drawable :RotateDrawable = layerDrawable?.findDrawableByLayerId(R.id.card) as RotateDrawable
 
-        val img = findViewById<ImageView>(R.id.imageView)
-
-        val layerDrawable: LayerDrawable = img.getDrawable() as LayerDrawable
-        val drawable = layerDrawable.getDrawable(0) as RotateDrawable
-
-        mAnimator = ObjectAnimator.ofInt(drawable, "level", 0, 10000)
-        mAnimator.duration = 3000
-        mAnimator.start()
-        mAnimator.addListener(object : Animator.AnimatorListener {
+        val animate = ObjectAnimator.ofInt(drawable, "level",  0, 10000)
+        animate.duration = 5000
+        animate.addListener(object : Animator.AnimatorListener {
             override fun onAnimationStart(animator: Animator) {
                 Log.i("animation", "onAnimationStart")
             }
 
             override fun onAnimationEnd(animator: Animator) {
-                img.setVisibility(View.GONE)
-                img.setVisibility(View.VISIBLE)
+                Log.i("animation", "onAnimationEnd")
             }
 
             override fun onAnimationCancel(animator: Animator) {
@@ -92,6 +100,8 @@ class MainActivity : AppCompatActivity() {
                 // do nothing
             }
         })
+        animate.start()
+
     }
 
 }
